1c024940b950bba1ebb7bb12f931c20bb07cc1b9
cetherdev <robert.hct@gmail.com>
2022-06-11 23:27:23 -0500

    feat: bootstrap, helpers

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,35 @@
-var gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const minifyCSS = require('gulp-minify-css');
+const concat = require('gulp-concat');
 
-gulp.task("hello", function () {
-  console.log("Hello!!");
+// npm install gulp --save --only=dev
+gulp.task('hello', async function () {
+	console.log('Hello!!');
 });
 
-gulp.task("sass", function () {
-  return gulp
-    .src("scss/**/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("public/stylesheets"));
+// npm install sass gulp-sass --save-dev
+gulp.task('sass', function () {
+	return gulp
+		.src('scss/**/*.scss')
+		.pipe(sass()) // Converts sass to css with gulp sass
+		.pipe(gulp.dest('public/stylesheets'));
 });
+
+// npm install gulp-minify-css
+gulp.task('style_min', function () {
+	return gulp
+		.src('scss/**/*.scss')
+		.pipe(sass())
+		.pipe(minifyCSS())
+		.pipe(concat('style_main_min.css'))
+		.pipe(gulp.dest('public/stylesheets'));
+});
+
+// gulp watch
+gulp.task(
+	'watch',
+	gulp.series('style_min', function () {
+		gulp.watch('scss/**/*.scss', gulp.series('style_min'));
+	})
+);
